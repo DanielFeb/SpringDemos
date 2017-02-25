c2bf4d9eebceb22514aeee4881d3a80b4f90f015
Daniel <daniel@DanieldeMacBook-Pro.local>
2017-02-25 19:10:03 +0800

    Collection property injection

--- src/main/java/indi/daniel/spdm/xmlbean/demo/beans/Stage.java
+++ src/main/java/indi/daniel/spdm/xmlbean/demo/beans/Stage.java
@@ -1,10 +1,28 @@
 package indi.daniel.spdm.xmlbean.demo.beans;
 
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Created by daniel on 2017/2/25.
  */
 public class Stage {
+    private List<IProgramme> programmeList;
 
-    public void start(){
+    public List<IProgramme> getProgrammeList() {
+        return programmeList;
+    }
+
+    public void setProgrammeList(List<IProgramme> programmeList) {
+        this.programmeList = programmeList;
+    }
+
+    public void startShows(){
+        Iterator<IProgramme> iterator = programmeList.iterator();
+        while (iterator.hasNext()){
+            iterator.next().start();
+        }
     }
 }
